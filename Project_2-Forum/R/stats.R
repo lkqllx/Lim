ec88f1f1419f767c85a24dc8b800a221f460f18d
lkqllx <lkqllx@gmail.com>
2019-10-25 10:44:35 +0800

    R for multiple years

--- Project_2-Forum/R/stats.R
+++ Project_2-Forum/R/stats.R
@@ -28,22 +28,37 @@ summ<- function (y){
   return(summt)
 }
 
-pnls = read.csv('/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/data/interim/daily_pnls.csv')
-# pnls$X <- as.Date(pnls$X)
+pnls = read.csv('/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/R/excess_ret/excess_daily_pnls.csv')
+reads = c('/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/R/excess_ret/excess_daily_pnls.csv',
+          '/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/R/excess_ret/2015.csv',
+          '/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/R/excess_ret/2016.csv',
+          '/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/R/excess_ret/2017.csv',
+          '/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/R/excess_ret/2018.csv',
+          '/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/R/excess_ret/2019.csv')
+writes = c('/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/R/stats.csv',
+           '/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/R/2015stats.csv',
+           '/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/R/2016stats.csv',
+           '/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/R/2017stats.csv',
+           '/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/R/2018stats.csv',
+           '/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/R/2019stats.csv')
 
-for (col in c(2,3,4,5)) {
-  # a <- cbind(pnls[,col])
-  # rownames(a) <- as.POSIXct(pnls[,1], format ='%Y-%m-%d')
-  curr_stats <- summ(cbind(pnls[,col]))
-  if (col == 2) {
-    all_stats <- curr_stats
-  } else{
-    all_stats <- cbind(all_stats, curr_stats)
+for (path_idx in 1:6){
+  pnls = read.csv(reads[path_idx])
+  all_stats = c()
+  for (col in c(2,3,4,5,6,7)) {
+    # a <- cbind(pnls[,col])
+    # rownames(a) <- as.POSIXct(pnls[,1], format ='%Y-%m-%d')
+    curr_stats <- summ(cbind(pnls[,col]))
+    if (col == 2) {
+      all_stats <- curr_stats
+    } else{
+      all_stats <- cbind(all_stats, curr_stats)
+    }
   }
+  
+  all_stats <- round(all_stats, 3)
+  colnames(all_stats) <- c('cmc3_ret', 'cmc5_ret', 'cmc10_ret', 'cmc15_ret', 'cmc20_ret', 'cmc30_ret')
+  write.csv(all_stats, writes[path_idx])
 }
-all_stats <- round(all_stats, 3)
-colnames(all_stats) <- c('cmc_ret', 'cmo_ret', 'omo_ret', 'CSI300')
-write.csv(all_stats, '/Users/andrew/Desktop/HKUST/Projects/Firm/LIM/Project_2-Forum/R/stats.csv')
-
 
 
